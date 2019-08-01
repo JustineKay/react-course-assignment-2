e88cf425010bcc59d948f85dcddf88b4681bf4a0
Justine Kay <justine.b.kitagorsky@accenture.com>
2019-08-01 17:03:24 -0400

    step 3

--- src/Validation/Validation.js
+++ src/Validation/Validation.js
@@ -3,7 +3,11 @@ import React from 'react';
 const validation = (props) => {
     return (
         <div>
-            <p>Longer text pleeeaaaase</p>
+            {
+                props.inputLength > 5 ?
+                <p>You're doing great (thumbs up)</p> :
+                <p>Longer text pleeeaaaase</p>
+            }
         </div>
     )
 };
